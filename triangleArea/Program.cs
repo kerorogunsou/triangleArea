281fe11e38b5fc625bd02b0f84fe60d28a9d3eb5
kerorogunsou <liubov.shamanina@yandex.ru>
2016-03-10 22:51:22 +0300

    Program computing area of a triangle with tests.

---
+++ triangleArea/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace DoubleExtensionMethods {
+    public static class DoubleExtension {
+        private static double precision = Math.Pow(10, -7);
+
+        public static bool AlmostEquals(this double double1, double double2) {
+            return (Math.Abs(double1 - double2) <= precision);
+        }
+    }
+}
+
+namespace Triangle {
+    using DoubleExtensionMethods;
+
+    public class RightTriangle {
+        double hypotenuse;
+        double cathetusA;
+        double cathetusB;
+        private bool? validResult;
+
+        public RightTriangle(double a, double b, double c) {
+            // finding hypotenuse as the longest side, and catheti as the shortest and second shortest sides
+            hypotenuse = Math.Max(a, Math.Max(b, c));
+            cathetusA = Math.Min(a, Math.Min(b, c));
+            cathetusB = a + b + c - hypotenuse - cathetusA;
+        }
+
+        public bool IsValid() {
+            // triangle is considered valid if it's sides's lengths >= 0 and it satisfies Pythagorean equation
+            if (validResult == null) {
+                validResult = AreAllSidesGreaterThanZero() && SatisfiesPythagoreanEquation();
+            }
+            return (bool) validResult;
+        }
+
+        public double GetAreaUsingHeronsEquation() {
+            if (IsValid() == false) {
+                throw new Exception("Triangle is not right");
+            }
+
+            double s = GetSemiperimeter();
+            return Math.Sqrt(s * (s - hypotenuse) * (s - cathetusA) * (s - cathetusB));
+        }
+
+        public double GetAreaUsingTwoCatets() {
+            if (IsValid() == false) {
+                throw new Exception("Triangle is not right");
+            }
+            return (cathetusA * cathetusB) / 2;
+        }
+
+        private double GetSemiperimeter() {
+            return (hypotenuse + cathetusA + cathetusB) / 2;
+        }
+
+        private bool AreAllSidesGreaterThanZero() {
+            // triangle is wrong if at least 1 side's length <= 0
+            return (hypotenuse > 0) && (cathetusA > 0) && (cathetusB > 0);
+        }
+
+        private bool SatisfiesPythagoreanEquation() {
+            // in the right triangle c^2 = a^2 + b^2, where c is hypotenuse
+            double expectedHypotenuse = Math.Sqrt(cathetusA * cathetusA + cathetusB * cathetusB);
+            return hypotenuse.AlmostEquals(expectedHypotenuse);
+        }
+    }
+}
---
+++ triangleAreaTests/UnitTest1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Triangle;
+using DoubleExtensionMethods;
+
+namespace triangleAreaTests {
+    [TestClass]
+    public class TriangleAreaTests {
+        [TestMethod]
+        public void Test1_CheckForPredefinedTriangle() {
+            double expectedArea = 6;
+            var triangle = new RightTriangle(5, 4, 3);
+
+            AssertAreaEqualsExpected(triangle, expectedArea);
+        }
+
+        [TestMethod]
+        public void Test2_CheckForWrongPredefinedTriangles() {
+            var triangle = new RightTriangle(5, 5, 3);
+            Assert.AreEqual(triangle.IsValid(), false, "Test2 failed: valid 1");
+
+            triangle = new RightTriangle(-5, -4, -3);
+            Assert.AreEqual(triangle.IsValid(), false, "Test2 failed: valid 2");
+
+            triangle = new RightTriangle(0, 0, 0);
+            Assert.AreEqual(triangle.IsValid(), false, "Test2 failed: valid 3");
+        }
+
+        [TestMethod]
+        public void Test3_CheckForRandomTriangles() {
+            Random rand = new Random();
+            double cathetusA, cathetusB;
+            for (int i = 0; i< 1000; i++) {
+                // generating random lengths for catheti in range of [0; 100]
+                cathetusA = (rand.NextDouble()) * 100;
+                cathetusB = (rand.NextDouble()) * 100;
+                double hypotenuse = Math.Sqrt(cathetusA * cathetusA + cathetusB * cathetusB);
+                double expectedArea = cathetusA * cathetusB / 2;
+
+                AssertAreaEqualsExpected(new RightTriangle(cathetusA, cathetusB, hypotenuse), expectedArea);
+            }
+        }
+
+        private void AssertAreaEqualsExpected(RightTriangle triangle, double expectedArea) {
+            var areaHeron = triangle.GetAreaUsingHeronsEquation();
+            var areaTwoCatets = triangle.GetAreaUsingTwoCatets();
+
+            Assert.AreEqual(areaHeron.AlmostEquals(areaTwoCatets) && areaHeron.AlmostEquals(expectedArea),
+                true,
+                "Alghortm results mismatch: heron={0} twoCatets={1} expeted={2}",
+                areaHeron, areaTwoCatets, expectedArea
+            );
+        }
+    }
+}
